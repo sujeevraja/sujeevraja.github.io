98d24ed043e1b0aff750ff0b98a0438c5cc554c7
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-05-04 10:26:09 -0700

    used for loop for links and content boxes

--- src/views/Content.js
+++ src/views/Content.js
@@ -3,14 +3,18 @@ var PublicationList = require("../views/PublicationList")
 
 module.exports = {
     view: function () {
-        var boxes = [
-            m('div', { class: 'outline w-100 pa3' }, 'bio box'),
-            m('div', { class: 'outline w-100 pa3 mt2' }, 'experience box'),
-            m('div', { class: 'outline w-100 pa3 mt2' }, 'education box'),
-            m('div', { class: 'outline w-100 pa3 mt2' }, m(PublicationList)),
-            m('div', { class: 'outline w-100 pa3 mt2' }, 'presentations box'),
-            m('div', { class: 'outline w-100 pa3 mt2' }, 'other box'),
-        ]
+        var contentBoxes = [
+            'bio box',
+            'experience box',
+            'education box',
+            m(PublicationList),
+            'presentations box',
+            'other box']
+
+        var boxes = contentBoxes.map(function (comp) {
+            return m('div', { class: 'shadow-1 br3 w-100 pa3 mt2' }, comp)
+        })
+
         return m('div', { class: 'flex flex-column w-100 ml2' }, boxes)
     }
 }
--- src/views/Sidebar.js
+++ src/views/Sidebar.js
@@ -2,33 +2,42 @@ var m = require("mithril").default;
 
 module.exports = {
     view: function () {
-        return m('div', { class: 'flex flex-column w6' }, [
-            m('div', { class: 'outline w-100 pa3 br3 dim bg-purple white' }, 'Bio'),
-            m('div', { class: 'outline w-100 pa3 mt2 br3 dim bg-purple white' }, 'Experience'),
-            m('div', { class: 'outline w-100 pa3 mt2 br3 dim bg-purple white' }, 'Education'),
-            m('div', { class: 'outline w-100 pa3 mt2 br3 dim bg-purple white' }, 'Publications'),
-            m('div', { class: 'outline w-100 pa3 mt2 br3 dim bg-purple white' }, 'Presentations'),
-            m('div', { class: 'outline w-100 pa3 mt2 br3 dim bg-purple white' }, 'Other'),
+        var iconSet1 = m('div', { class: 'w-100 mt2 flex' }, [
+            m('div', { class: 'w-third pa1 dim' },
+                m('a', { href: 'https://scholar.google.com/citations?user=S2CWoKoAAAAJ&hl=en' },
+                    m('i', { class: 'ai ai-google-scholar-square ai-2x' }))),
 
-            m('div', { class: 'w-100 mt2 flex' }, [
-                m('div', { class: 'w-third pa1 dim' },
-                    m('a', { href: 'https://scholar.google.com/citations?user=S2CWoKoAAAAJ&hl=en' },
-                        m('i', { class: 'ai ai-google-scholar-square ai-2x' }))),
+            m('div', { class: 'w-third pa1 dim' },
+                m('a', { href: 'https://www.linkedin.com/in/sujeevraja-sanjeevi-2b434242' },
+                    m('i', { class: 'fab fa-linkedin-in fa-2x' }))),
 
-                m('div', { class: 'w-third pa1 dim' },
-                    m('a', { href: 'https://www.linkedin.com/in/sujeevraja-sanjeevi-2b434242' },
-                        m('i', { class: 'fab fa-linkedin-in fa-2x' }))),
-
-                m('div', { class: 'w-third pa1 dim' },
-                    m('a', { href: 'https://github.com/sujeevraja' },
-                        m('i', { class: 'fab fa-github fa-2x' })))
-            ]),
-
-            m('div', { class: 'w-100 mt2 flex' }, [
-                m('div', { class: 'w-third pa1 dim' },
-                    m('a', { href: 'https://www.researchgate.net/profile/Sujeevraja_Sanjeevi' },
-                        m('i', { class: 'fab fa-researchgate fa-2x' })))
-            ]),
+            m('div', { class: 'w-third pa1 dim' },
+                m('a', { href: 'https://github.com/sujeevraja' },
+                    m('i', { class: 'fab fa-github fa-2x' })))
         ])
+
+        var iconSet2 = m('div', { class: 'w-100 mt2 flex' }, [
+            m('div', { class: 'w-third pa1 dim' },
+                m('a', { href: 'https://www.researchgate.net/profile/Sujeevraja_Sanjeevi' },
+                    m('i', { class: 'fab fa-researchgate fa-2x' })))
+        ])
+
+        var links = [
+            'Bio',
+            'Experience',
+            'Education',
+            'Publications',
+            'Presentations',
+            'Other'
+        ]
+
+        var sidebarBoxes = links.map(function (link) {
+            return m('div', {
+                class: 'outline w-100 pa3 mt2 br3 dim bg-purple white'
+            }, link)
+        })
+
+        sidebarBoxes = sidebarBoxes.concat([iconSet1, iconSet2])
+        return m('div', { class: 'flex flex-column w6' }, sidebarBoxes)
     }
 }
