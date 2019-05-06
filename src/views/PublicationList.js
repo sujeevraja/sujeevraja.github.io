bd22e89cb7961f7f5dd7832e8055dc771114d2ab
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-05-05 17:29:42 -0700

    added working publications

--- src/views/PublicationList.js
+++ src/views/PublicationList.js
@@ -2,15 +2,25 @@ var m = require("mithril").default
 var Publication = require("../models/Publication")
 
 var publicationView = function (pub) {
-    var s = (pub.authors + ", Vol "
-        + pub.volume + ", Issue " + pub.issue + ", " + pub.pages + ", "
-        + pub.year + ".")
-    return m("li", { class: "pa2" }, [
+    var children = [
         m('i', pub.title),
         ", ",
-        m('b', pub.journal),
-        ", ",
-        s,])
+        pub.authors,
+        ", "]
+
+    if (pub.hasOwnProperty("journal")) {
+        children = children.concat([
+            m('b', pub.journal),
+            ", Vol ", pub.volume,
+            ", Issue ", pub.issue,
+            ", ", pub.pages,
+            ", ", pub.year,
+            "."])
+    } else {
+        children = children.concat(["(working)."])
+    }
+
+    return m("li", { class: "pa2" }, children)
 }
 
 module.exports = {
