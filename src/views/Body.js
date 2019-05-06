5d7e3a82c6bdc54d0fba01cff6e6737519a802c3
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-05-05 22:03:34 -0700

    added header

--- src/views/Body.js
+++ src/views/Body.js
@@ -3,7 +3,10 @@ var Sidebar = require("./Sidebar")
 var Content = require("./Content")
 
 var view = function (vnode) {
-    return m('div', { class: 'helvetica pa2' }, [
+    var column = m('div', { class: 'bg-orange white pa3 tc' },
+        m('a', { href: '/', class: 'no-underline white f4' }, "Sujeevraja (Sujeev) Sanjeevi"))
+    return m('div', { class: 'helvetica' }, [
+        column,
         m('div', { class: 'flex items-start' }, [
             m(Sidebar), // m(component) consumes the component to generate a view.
             m(Content, { comp: vnode.attrs.comp }),
--- src/views/Content.js
+++ src/views/Content.js
@@ -3,7 +3,7 @@ var m = require("mithril").default
 module.exports = {
     view: function (vnode) {
         return m('div', {
-            class: 'shadow-1 br3 w-100 pa3 mt2 ml2',
+            class: 'shadow-1 br3 w-100 pa3 mt2 ml3 mr2',
             id: 'content'
         }, m(vnode.attrs.comp))
     }
