f29b029df95530c6e65ae1611fe1f4fe8926adb6
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2022-02-11 09:21:47 -0700

    finish bio page update

--- src/views/Link.js
+++ src/views/Link.js
@@ -2,9 +2,8 @@ var m = require("mithril")
 
 module.exports = {
     view: function (vnode) {
-        var name = vnode.attrs.name
-        var url = vnode.attrs.url
-
-        return m('a', { "href": vnode.attrs.url }, vnode.attrs.name)
+        return m('a',
+            { "href": vnode.attrs.url },
+            vnode.attrs.name)
     }
 }
